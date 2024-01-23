07831fc5b73ee9d6f71aa3310d6d4ebf99e56781
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 11:11:55 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,10 @@
 export function Logo(props) {
 
    return (
-    <span style="white-space:nowrap">
+    <div>
           <img src="origin-logo.png" width="20px" alt="" />
-      <div>
-    <b>ORIGIN</b>
+          <b>ORIGIN</b> / Investigation / Intelligence
     </div>
-</span>
     
   )
 }
