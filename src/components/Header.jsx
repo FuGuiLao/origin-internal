f8e2b82acd109eed7b4d8b21333cd43c467acdee
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-22 18:00:53 -0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,9 +68,9 @@ export const Header = forwardRef(function Header({ className }, ref) {
       <div className="flex items-center gap-5">
         <nav className="hidden md:block">
           <ul role="list" className="flex items-center gap-8">
-            <TopLevelNavItem href="/">API</TopLevelNavItem>
-            <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
-            <TopLevelNavItem href="#">Support</TopLevelNavItem>
+            <TopLevelNavItem href="/">Home</TopLevelNavItem>
+            <TopLevelNavItem href="https://www.origininvestigations.com">Origin / Investigation</TopLevelNavItem>
+            <TopLevelNavItem href="https://www.originintelligence.org">ORIGIN / Intelligence</TopLevelNavItem>
           </ul>
         </nav>
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
