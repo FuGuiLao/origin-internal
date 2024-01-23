3cd034aab0d88b94eb396fe509ea4c54970378d0
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 11:38:44 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -199,7 +199,6 @@ function SmallPrint() {
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
         &copy; Copyright {new Date().getFullYear()} Origin Investigations Inc. All Rights Reserved.
-        <span>    <img src="origin-logo.png" width="25px" alt="" /></span>
       </p>
     </div>
   )
