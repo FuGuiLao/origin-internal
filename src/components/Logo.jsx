bfa7d8282667ea6abcc59ae24fb81fca334e29f9
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 11:11:03 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@
 export function Logo(props) {
 
    return (
-    <span style="white-space:nowrap;">
+    <span style="white-space:nowrap">
           <img src="origin-logo.png" width="20px" alt="" />
       <div>
     <b>ORIGIN</b>
