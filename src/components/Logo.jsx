37a12e459a4fa62d8d8dc63cd10768a7f294adcd
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 10:40:14 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,9 @@
+import logo from '@/images/logo/origin-logo.png'
+
 export function Logo(props) {
    return (
     <div>
+       <Image src={logo} alt="" unoptimized />
    <b>ORIGIN</b> / Investigation / Intelligence
     </div>
 
