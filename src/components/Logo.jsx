9d77da6ad51a8ce65e44c348ba6852a9d7f3dee3
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 10:57:34 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,7 @@ export function Logo(props) {
 
    return (
     <div>
-       <img src="origin-logo.png" width="40px" alt="" unoptimized />
-   <b>ORIGIN</b>
+       <img src="origin-logo.png" width="20px" alt="" /><b>ORIGIN</b>
     </div>
 
     
