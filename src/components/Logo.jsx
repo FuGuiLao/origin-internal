68d07f125f11481cc7654efaae30e1fee5a32ee1
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 10:29:03 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,7 @@
 export function Logo(props) {
    return (
-    <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
-      <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        &copy; Copyright {new Date().getFullYear()} Origin Investigations Inc. All Rights Reserved.
-      </p>
+    <div>
+   ORIGIN / Investigation / Intelligence
     </div>
 
     
