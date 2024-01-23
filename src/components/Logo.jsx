92f020937d3ae034e3b4b25f39357c5c60f90337
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-01-23 11:53:22 -0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,6 +6,9 @@ export function Logo(props) {
     <div className="text-zinc-900 dark:hidden">
           <b>ORIGIN</b> / Investigation / Intelligence
     </div>
+          <div className="dark:text-white light:hidden">
+          <b>ORIGIN</b> / Investigation / Intelligence
+    </div>
     
   )
 }
