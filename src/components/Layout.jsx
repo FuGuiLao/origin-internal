b4f744088efe569b2e084229456b059bd32746b7
alpinistdev <tony715225@gmail.com>
2024-01-30 10:27:59 +0800

    Added login logic

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,32 +7,54 @@ import { Logo } from '@/components/Logo'
 import { Navigation } from '@/components/Navigation'
 import { Prose } from '@/components/Prose'
 import { SectionProvider } from '@/components/SectionProvider'
+import { Login } from './Login'
+import { useEffect, useState } from 'react'
 
 export function Layout({ children, sections = [] }) {
+  const [isLogged, setIsLogged] = useState(false)
+  useEffect(() => {
+    const currentTime = new Date().getTime()
+    const expiresAt = sessionStorage.getItem('expiresAt')
+    const token = sessionStorage.getItem('token')
+    if (token) {
+      if (currentTime > expiresAt) {
+        sessionStorage.removeItem('token')
+        sessionStorage.removeItem('expiresAt')
+        setIsLogged(false)
+      } else {
+        setIsLogged(true)
+      }
+    } else {
+      setIsLogged(false)
+    }
+  }, [])
   return (
-    <SectionProvider sections={sections}>
-      <div className="lg:ml-72 xl:ml-80">
-        <motion.header
-          layoutScroll
-          className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
-        >
-          <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
-            <div className="hidden lg:flex">
-              <Link href="/" aria-label="Home">
-                <Logo className="h-6" />
-              </Link>
+    <>
+      <SectionProvider sections={sections}>
+        <div className="lg:ml-72 xl:ml-80">
+          <motion.header
+            layoutScroll
+            className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
+          >
+            <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
+              <div className="hidden lg:flex">
+                <Link href="/" aria-label="Home">
+                  <Logo className="h-6" />
+                </Link>
+              </div>
+              <Header />
+              <Navigation className="hidden lg:mt-10 lg:block" />
             </div>
-            <Header />
-            <Navigation className="hidden lg:mt-10 lg:block" />
+          </motion.header>
+          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+            <main className="py-16">
+              <Prose as="article">{children}</Prose>
+            </main>
+            <Footer />
           </div>
-        </motion.header>
-        <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
-          <main className="py-16">
-            <Prose as="article">{children}</Prose>
-          </main>
-          <Footer />
         </div>
-      </div>
-    </SectionProvider>
+      </SectionProvider>
+      {!isLogged && <Login />}
+    </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+export function Login() {
+  const [password, setPassword] = useState('')
+
+  const handleClick = () => {
+    if (password === '123') {
+      sessionStorage.setItem('token', 'confirmed')
+      sessionStorage.setItem('expiresAt', new Date().getTime() + 60 * 60 * 1000)
+      location.href = '/'
+    }
+  }
+  return (
+    <div className="fixed left-1/2 top-1/2 z-50 block h-full w-full -translate-x-1/2 -translate-y-1/2 bg-white/20 backdrop-blur-sm">
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="shadow-white-700 z-10 flex w-[350px] justify-between rounded-xl bg-white shadow-md shadow-slate-950">
+          <div className="m-4">
+            <input
+              type="password"
+              className="w-full border px-2 py-1 text-lg placeholder-gray-400 focus:ring-0 focus:ring-offset-0"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Input password here!"
+            />
+          </div>
+          <div className="m-4">
+            <button
+              className="rounded-lg bg-slate-700 px-3 py-2 text-sm font-medium text-white hover:bg-slate-900"
+              onClick={handleClick}
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
