6d390a1de8628729bac158229115d0c9a3f7a68c
FuGuiLao <EAGLEMINY@GMAIL.COM>
2025-01-31 21:21:57 -0500

    Minor Change
    Changed Microsoft Provider to Azure Provider

--- src/pages/api/auth/[...]nextauth.js
+++ src/pages/api/auth/[...]nextauth.js
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
-import MicrosoftProvider from "next-auth/providers/microsoft";
+import AzureADProvider from "next-auth/providers/azure-ad";
 
 export default NextAuth({
     providers: [
-        MicrosoftProvider({
-            clientId: process.env.MICROSOFT_CLIENT_ID,
-            clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
+        AzureADProvider({
+            clientId: process.env.AZURE_AD_CLIENT_ID,
+            clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+            tenantId: process.env.AZURE_AD_TENANT_ID, // Optional, use 'common' if unsure
             authorization: { params: { scope: "openid profile email User.Read" } },
         }),
     ],
@@ -15,7 +16,7 @@ export default NextAuth({
         async jwt({ token, account, profile }) {
             if (account) {
                 token.accessToken = account.access_token;
-                token.id = profile.id;
+                token.id = profile.oid || profile.id; // Use `oid` for Azure profiles
             }
             return token;
         },
